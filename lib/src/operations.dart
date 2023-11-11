26b4312de4a8b12fa09cff43dc72c688774be41e
csandovalgt <carlossmetal@gmail.com>
2023-11-11 06:47:58 -0600

    Initial commit

---
+++ lib/src/operations.dart
@@ -0,0 +1,18 @@
+int suma(int a, int b) {
+  return a + b;
+}
+
+int resta(int a, int b) {
+  return a - b;
+}
+
+int multiplicacion(int a, int b) {
+  return a * b;
+}
+
+double division(int a, int b) {
+  if (b == 0) {
+    throw ArgumentError('No se puede dividir por cero');
+  }
+  return a / b;
+}
